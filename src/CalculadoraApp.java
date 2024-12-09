6ab07533d8faa312fd07dd17f275c1a1011f9a37
Israel Estrada <israel@compilaideas.com>
2024-12-08 19:25:05 -0600

    A new instance of Scanner() to obtain data input. Switch function for menu functionality If else conditionals for basic logic 1 to 4 options.

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -2,13 +2,42 @@ import java.util.Scanner;
 
 public class CalculadoraApp {
     public static void main(String[] args) {
-        System.out.println("**** Calculator App ***\n");
-        System.out.println("Type operator 1: ");
         Scanner console = new Scanner(System.in);
-        var operator1 = Integer.parseInt(console.nextLine());
-        System.out.println("Type operator 2: ");
-        var operator2 = Integer.parseInt(console.nextLine());
-        var result = operator1 + operator2;
-        System.out.println("Result: " + result);
+        System.out.println("**** Calculator App ***\n");
+        // Show menu
+        System.out.println("""
+                1. Sum
+                2. Rest
+                3. Multiplication 
+                4. Division 
+                5. Exit
+                """);
+        System.out.print("Type an option: ");
+        var operation = Integer.parseInt(console.nextLine());
+        // Check options range
+        if (operation >= 1 && operation < 5) {
+            System.out.print("Type operator 1: ");
+            var operator1 = Integer.parseInt(console.nextLine());
+            System.out.print("Type operator 2: ");
+            var operator2 = Integer.parseInt(console.nextLine());
+
+            int result = 0;
+            switch (operation) {
+                case 1 ->
+                    result = operator1 + operator2;
+                case 2 ->
+                    result = operator1 - operator2;
+                case 3 ->
+                    result = operator1 * operator2;
+                case 4 ->
+                    result = operator1 / operator2;
+                default ->  System.out.println("Invalid operation");
+            }
+            System.out.println("Result: " + result);
+        } else if (operation == 5) {
+            System.out.println("End...");
+        } else {
+            System.out.println("Invalid operation");
+        }
     }
 }
