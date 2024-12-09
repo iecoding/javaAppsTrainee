ae8bccf04256de5326c31f074507af39905575c3
Israel Estrada <israel@compilaideas.com>
2024-12-08 19:00:22 -0600

    Simple inputs

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class CalculadoraApp {
+    public static void main(String[] args) {
+        System.out.println("**** Calculator App ***\n");
+        System.out.println("Type operator 1: ");
+        Scanner console = new Scanner(System.in);
+        var operator1 = Integer.parseInt(console.nextLine());
+        System.out.println("Type operator 2: ");
+        var operator2 = Integer.parseInt(console.nextLine());
+        var result = operator1 + operator2;
+        System.out.println("Result: " + result);
+    }
 }
